cf635ee364605cc8cb1936ebdfc0f646d77f925b
Alexis Lopez Zubieta <contact@azubieta.net>
2019-07-31 17:22:05 -0500

    Move helpers targets to a separate cmake script

---
+++ src/installer/Helpers.cmake
@@ -0,0 +1,23 @@
+add_executable(install-helper install-helper.cpp)
+target_link_libraries(install-helper AppImageServicesInstaller)
+install(TARGETS install-helper RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR})
+
+add_executable(uninstall-helper uninstall-helper.cpp)
+target_link_libraries(uninstall-helper AppImageServicesInstaller)
+install(TARGETS uninstall-helper RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR})
+
+###
+# Self install scripts
+###
+install(
+        PROGRAMS appimage-services-installer.sh appimage-services-uninstaller.sh
+        DESTINATION ${CMAKE_INSTALL_BINDIR}
+)
+
+install(
+        PROGRAMS
+        utils/update_launchers_desktop_entries.sh
+        utils/debug-header.sh
+        utils/settings.sh
+        DESTINATION ${CMAKE_INSTALL_BINDIR}/utils
+)
